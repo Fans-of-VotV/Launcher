896d4f7f175f4af633791dfaa194798ce782a531
Arseniy Savchenko <arseny.saw4enko@gmail.com>
2026-02-22 03:41:17 +0300

    Change index to be signed int

--- Source/Launcher/CommandLine.cpp
+++ Source/Launcher/CommandLine.cpp
@@ -4,7 +4,7 @@ void CommandLine::Parse() {
   int argc = 0;
   wchar_t** argv = CommandLineToArgvW(GetCommandLineW(), &argc);
 
-  for (uint i = 1; i < argc; ++i) {
+  for (int i = 1; i < argc; ++i) {
     std::wstring arg = argv[i];
 
     if (arg == L"--localhost") {
