0cf93ad1d2f3e71dc64fafa6ff950d5053da2435
Arseniy Savchenko <arseny.saw4enko@gmail.com>
2026-02-22 03:17:59 +0300

    Add basic WebView typescript declarations

---
+++ Frontend/src/types/chrome.d.ts
@@ -0,0 +1,8 @@
+declare global {
+  interface Chrome {}
+
+  var chrome: Chrome;
+}
+
+export {};
+
--- Frontend/src/types/webview.d.ts
+++ Frontend/src/types/webview.d.ts
@@ -1,9 +1,28 @@
 declare global {
-  interface Window {
-    chrome?: {
-      webview?: {
-      };
-    };
+  interface WebViewMessageEvent extends MessageEvent {
+    additionalObjects: ArrayLike<FileSystemFileHandle | FileSystemDirectoryHandle | null>;
+    source: WebView;
+  }
+
+  interface SharedBufferReceivedEvent extends Event {
+    additionalData: any | undefined;
+    source: WebView;
+
+    getBuffer(): ArrayBuffer;
+  }
+
+  interface WebView {
+    addEventListener(type: "message", listener: (event: WebViewMessageEvent) => void): void;
+    addEventListener(type: "sharedbufferreceived", listener: (event: SharedBufferReceivedEvent) => void): void;
+    removeEventListener(type: "message", listener: (event: WebViewMessageEvent) => void);
+    removeEventListener(type: "sharedbufferreceived", listener: (event: SharedBufferReceivedEvent) => void): void;
+    postMessage(message: any): void;
+    postMessageWithAdditionalObjects(message: any, additionalObjects: ArrayLike<File>): void;
+    releaseBuffer(buffer: ArrayBuffer): void;
+  }
+
+  interface Chrome {
+    webview: WebView;
   }
 }
 
